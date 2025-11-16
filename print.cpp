a2109a0e2fd2982fdfa874df734a11732583c9e7
MMunim <mmunim9.9.01@gmail.com>
2025-11-16 13:08:20 +0600

    Print on c++

---
+++ print.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+int main()
+{
+    // std :: cout << "Hello World!";
+
+    int x = 999;
+    char c = 'm';
+    double d = 34.55;
+
+    std :: cout << x << std::endl << c << std::endl << d << std::endl;
+
+    std :: cout << x << " " << c << " " << d << "\n";
+
+
+    // my favourite number is 999
+    std:: cout<< "My Favourite number is: " << x << std:: endl;
+    return 0;
+}
