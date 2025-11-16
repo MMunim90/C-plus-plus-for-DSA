83279b6c19750bcd4ca307a23890a7a6c38e3420
MMunim <mmunim9.9.01@gmail.com>
2025-11-16 18:58:24 +0600

    swap function on c++

---
+++ min_max_swap.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    // if(a<b)
+    // {
+    //     cout << a << endl;
+    // }
+    // else
+    // {
+    //     cout << b << endl;
+    // }
+
+    cout << min(a,b) << endl;
+    cout << max(a,b) << endl;
+    
+    cout << min({1, 8, 3, 4, 9, 6, 4, 8, 3, 4}) << endl;
+    cout << max({1, 8, 3, 4, 9, 6, 4, 8, 3, 4}) << endl;
+
+    //swap function
+    swap(a,b);
+    cout << a << " " << b << endl;
+
+    return 0;
+}
