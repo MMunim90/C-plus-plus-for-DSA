b45e55f7070cdfbb979529db51539ef1cb6a9f6f
MMunim <mmunim9.9.01@gmail.com>
2025-11-17 18:45:13 +0600

    explore dynamic memory concept

---
+++ Module 2/dynamic_variable.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int *p;
+int *q;
+
+// static variable
+void func()
+{
+    int x = 10;
+    p = &x;
+    cout << "Func => " << *p << endl;
+    return;
+}
+
+//dynamic variable
+void func2()
+{
+    int* y = new int;
+    *y = 100;
+    q = y;
+    cout << "Fun -> " << *q << endl;
+    return;
+}
+
+int main()
+{
+    printf("Static Variable\n");
+    func();
+    cout << "Main => " << *p << endl;
+    printf("\n");
+    printf("Dynamic Variable\n");
+    func2();
+    cout << "Main => " << *q << endl;
+    // int x = 10;
+    // int *p = new int;
+    // *p = 100;
+    // cout << *p << endl << &p << endl << p << endl;
+    return 0;
+}
