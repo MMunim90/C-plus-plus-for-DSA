46535a722c1bb6fb70b31ec388d1855924452171
MMunim <mmunim9.9.01@gmail.com>
2025-11-27 14:58:15 +0600

    explore string capacity functions

---
+++ Module 5/string_capacity.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "munim want be a problem solver";
+    // cout << s.size() << endl;
+    // cout << s.max_size() << endl;
+    // cout << s.capacity() << endl;
+
+    // s.clear();
+
+    // if(s.empty() == true)
+    // {
+    //     cout << "Empty" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Not Empty" << endl;
+    // }
+
+    // if(s.size() == 0)
+    // {
+    //     cout << "Empty" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Not Empty" << endl;
+    // }
+
+
+    // s.resize(35, 'r');
+    s.resize(3);
+
+    cout << s << endl;
+    cout << s.size() << endl;
+    return 0;
+}
