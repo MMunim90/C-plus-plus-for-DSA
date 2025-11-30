dbe12a29c3cfb30029fdc2bf1eb2afb153fa4e57
MMunim <mmunim9.9.01@gmail.com>
2025-11-30 17:20:24 +0600

    quiz done

---
+++ Module 7/quiz.cpp
@@ -0,0 +1,54 @@
+// 1
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// class Student
+// {
+//     public:
+//     int roll;
+//     Student(int r)
+//     {
+//         roll = r;
+//     }
+// };
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+
+//     Student a = new Student a[n];
+
+//     cin >> a.  // not possible
+//     return 0;
+// }
+
+
+// 2
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// class Student
+// {
+//     public:
+//     int roll;
+// };
+
+// int main()
+// {
+//     Student a[5];
+
+//     for(int i=0; i<5; i++)
+//     {
+//         cin >> a[i].roll;
+//     }
+
+//     for(int i=0; i<5; i++)
+//     {
+//         cout << a[i].roll << " ";
+//     }
+//     return 0;
+// }
+
+
