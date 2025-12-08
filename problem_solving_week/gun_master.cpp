8bc03b232e2301b74211d0d9146e774de33dbc7a
MMunim <mmunim9.9.01@gmail.com>
2025-12-08 14:34:39 +0600

    room allocation problem solved

---
+++ problem_solving_week/gun_master.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n, range;
+        cin >> n >> range;
+
+        int target[n];
+        for(int i=0; i<n; i++)
+        {
+            cin >> target[i];
+        }
+
+        
+    }
+    
+    return 0;
+}
--- problem_solving_week/room_allocation.cpp
+++ problem_solving_week/room_allocation.cpp
@@ -8,20 +8,18 @@ int main()
     {
         int n;
         cin >> n;
-        int arr[n];
+        double arr[n];
         for(int i=0; i<n; i++)
         {
             cin >> arr[i];
         }
-
-        double total_room = 0;
-        double peoples = 0;
+        
+        int total_room = 0;
+        
         for(int i=0; i<n; i++)
         {
-            peoples += arr[i];
+            total_room += ceil(arr[i] / 2);
         }
-
-        total_room = ceil(peoples / 2);
 
         cout << total_room << endl;
     }
