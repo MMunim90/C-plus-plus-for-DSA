cd4235ca5ffd86bc481e94aa5a9619f5ab655957
MMunim <mmunim9.9.01@gmail.com>
2025-12-10 18:10:19 +0600

    squid game problem solved

---
+++ Extra Problem Solve in Codechef/squid_game.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x, y, prize;
+    cin >> x >> y;
+
+    prize = (x - y) * 10000;
+
+    cout << prize << endl;
+    return 0;
+}
