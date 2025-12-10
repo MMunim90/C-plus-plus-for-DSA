023a099551adfb83cec422a3d86dd07bdadece96
MMunim <mmunim9.9.01@gmail.com>
2025-12-10 22:41:29 +0600

    endless play problem solved

---
+++ Extra Problem Solve in Codechef/endless_play.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x, h, total_hour; cin >> x >> h;
+
+    total_hour = (x - 4) * 24 + h;
+
+    cout << total_hour << endl;
+    return 0;
+}
