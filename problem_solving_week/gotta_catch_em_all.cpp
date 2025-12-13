c6b46b713a1973acd2c71ea87c75e540b02c2c4f
MMunim <mmunim9.9.01@gmail.com>
2025-12-13 17:12:09 +0600

    yoga class problem solved

---
+++ problem_solving_week/gotta_catch_em_all.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n, x, y; cin >> n >> x >> y;
+        int arr[n];
+
+        for(int i=0; i<n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+
+        cout << endl;
+    }
+    
+    return 0;
+}
