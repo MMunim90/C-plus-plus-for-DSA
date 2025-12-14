171f80072672d747025abed44871949f3c3e1f32
MMunim <mmunim9.9.01@gmail.com>
2025-12-14 13:07:23 +0600

    cookie day problem solved

--- problem_solving_week/cookey_day.cpp
+++ problem_solving_week/cookey_day.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
@@ -11,30 +11,36 @@ int main()
 
         int cookies[jars];
 
-        for(int i=0; i<jars; i++)
+        for (int i = 0; i < jars; i++)
         {
             cin >> cookies[i];
         }
 
         int min_cookies_waste = INT_MAX;
         int min = 0;
-
-        for(int i=0; i<jars; i++)
+        int cooie_found = 0;
+        for (int i = 0; i < jars; i++)
         {
-            // cout << cookies[i] << " ";
-            
+            if (cookies[i] >= children)
+            {
                 min = cookies[i] % children;
-                if(min_cookies_waste > min)
+                if (min_cookies_waste > min)
                 {
                     min_cookies_waste = min;
                 }
-
-            
-
+                cooie_found = 1;
+            }
         }
 
-        cout << min_cookies_waste << endl;
+        if (cooie_found)
+        {
+            cout << min_cookies_waste << endl;
+        }
+        else
+        {
+            cout << "-1" << endl;
+        }
     }
-    
+
     return 0;
 }
